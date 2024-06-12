b656ce7a620ac9a8123aced5f2b10e1885a71bb0
Eugene Krevenets <ievgenii.krevenets@gmail.com>
2024-06-12 01:20:16 -0700

    chore: fix python style

--- src/INA219.py
+++ src/INA219.py
@@ -2,63 +2,67 @@ import smbus
 import time
 
 # Config Register (R/W)
-_REG_CONFIG                 = 0x00
+_REG_CONFIG = 0x00
 # SHUNT VOLTAGE REGISTER (R)
-_REG_SHUNTVOLTAGE           = 0x01
+_REG_SHUNTVOLTAGE = 0x01
 
 # BUS VOLTAGE REGISTER (R)
-_REG_BUSVOLTAGE             = 0x02
+_REG_BUSVOLTAGE = 0x02
 
 # POWER REGISTER (R)
-_REG_POWER                  = 0x03
+_REG_POWER = 0x03
 
 # CURRENT REGISTER (R)
-_REG_CURRENT                = 0x04
+_REG_CURRENT = 0x04
 
 # CALIBRATION REGISTER (R/W)
-_REG_CALIBRATION            = 0x05
+_REG_CALIBRATION = 0x05
+
 
 class BusVoltageRange:
     """Constants for ``bus_voltage_range``"""
-    RANGE_16V               = 0x00      # set bus voltage range to 16V
-    RANGE_32V               = 0x01      # set bus voltage range to 32V (default)
+    RANGE_16V = 0x00  # set bus voltage range to 16V
+    RANGE_32V = 0x01  # set bus voltage range to 32V (default)
+
 
 class Gain:
     """Constants for ``gain``"""
-    DIV_1_40MV              = 0x00      # shunt prog. gain set to  1, 40 mV range
-    DIV_2_80MV              = 0x01      # shunt prog. gain set to /2, 80 mV range
-    DIV_4_160MV             = 0x02      # shunt prog. gain set to /4, 160 mV range
-    DIV_8_320MV             = 0x03      # shunt prog. gain set to /8, 320 mV range
+    DIV_1_40MV = 0x00  # shunt prog. gain set to  1, 40 mV range
+    DIV_2_80MV = 0x01  # shunt prog. gain set to /2, 80 mV range
+    DIV_4_160MV = 0x02  # shunt prog. gain set to /4, 160 mV range
+    DIV_8_320MV = 0x03  # shunt prog. gain set to /8, 320 mV range
+
 
 class ADCResolution:
     """Constants for ``bus_adc_resolution`` or ``shunt_adc_resolution``"""
-    ADCRES_9BIT_1S          = 0x00      #  9bit,   1 sample,     84us
-    ADCRES_10BIT_1S         = 0x01      # 10bit,   1 sample,    148us
-    ADCRES_11BIT_1S         = 0x02      # 11 bit,  1 sample,    276us
-    ADCRES_12BIT_1S         = 0x03      # 12 bit,  1 sample,    532us
-    ADCRES_12BIT_2S         = 0x09      # 12 bit,  2 samples,  1.06ms
-    ADCRES_12BIT_4S         = 0x0A      # 12 bit,  4 samples,  2.13ms
-    ADCRES_12BIT_8S         = 0x0B      # 12bit,   8 samples,  4.26ms
-    ADCRES_12BIT_16S        = 0x0C      # 12bit,  16 samples,  8.51ms
-    ADCRES_12BIT_32S        = 0x0D      # 12bit,  32 samples, 17.02ms
-    ADCRES_12BIT_64S        = 0x0E      # 12bit,  64 samples, 34.05ms
-    ADCRES_12BIT_128S       = 0x0F      # 12bit, 128 samples, 68.10ms
+    ADCRES_9BIT_1S = 0x00  # 9bit,   1 sample,     84us
+    ADCRES_10BIT_1S = 0x01  # 10bit,   1 sample,    148us
+    ADCRES_11BIT_1S = 0x02  # 11 bit,  1 sample,    276us
+    ADCRES_12BIT_1S = 0x03  # 12 bit,  1 sample,    532us
+    ADCRES_12BIT_2S = 0x09  # 12 bit,  2 samples,  1.06ms
+    ADCRES_12BIT_4S = 0x0A  # 12 bit,  4 samples,  2.13ms
+    ADCRES_12BIT_8S = 0x0B  # 12bit,   8 samples,  4.26ms
+    ADCRES_12BIT_16S = 0x0C  # 12bit,  16 samples,  8.51ms
+    ADCRES_12BIT_32S = 0x0D  # 12bit,  32 samples, 17.02ms
+    ADCRES_12BIT_64S = 0x0E  # 12bit,  64 samples, 34.05ms
+    ADCRES_12BIT_128S = 0x0F  # 12bit, 128 samples, 68.10ms
+
 
 class Mode:
     """Constants for ``mode``"""
-    POWERDOW                = 0x00      # power down
-    SVOLT_TRIGGERED         = 0x01      # shunt voltage triggered
-    BVOLT_TRIGGERED         = 0x02      # bus voltage triggered
-    SANDBVOLT_TRIGGERED     = 0x03      # shunt and bus voltage triggered
-    ADCOFF                  = 0x04      # ADC off
-    SVOLT_CONTINUOUS        = 0x05      # shunt voltage continuous
-    BVOLT_CONTINUOUS        = 0x06      # bus voltage continuous
-    SANDBVOLT_CONTINUOUS    = 0x07      # shunt and bus voltage continuous
+    POWERDOW = 0x00  # power down
+    SVOLT_TRIGGERED = 0x01  # shunt voltage triggered
+    BVOLT_TRIGGERED = 0x02  # bus voltage triggered
+    SANDBVOLT_TRIGGERED = 0x03  # shunt and bus voltage triggered
+    ADCOFF = 0x04  # ADC off
+    SVOLT_CONTINUOUS = 0x05  # shunt voltage continuous
+    BVOLT_CONTINUOUS = 0x06  # bus voltage continuous
+    SANDBVOLT_CONTINUOUS = 0x07  # shunt and bus voltage continuous
 
 
 class INA219:
     def __init__(self, i2c_bus=1, addr=0x40):
-        self.bus = smbus.SMBus(i2c_bus);
+        self.bus = smbus.SMBus(i2c_bus)
         self.addr = addr
 
         # Set chip to known config values to start
@@ -67,15 +71,15 @@ class INA219:
         self._power_lsb = 0
         self.set_calibration_32V_2A()
 
-    def read(self,address):
+    def read(self, address):
         data = self.bus.read_i2c_block_data(self.addr, address, 2)
-        return ((data[0] * 256 ) + data[1])
+        return ((data[0] * 256) + data[1])
 
-    def write(self,address,data):
-        temp = [0,0]
+    def write(self, address, data):
+        temp = [0, 0]
         temp[1] = data & 0xFF
-        temp[0] =(data & 0xFF00) >> 8
-        self.bus.write_i2c_block_data(self.addr,address,temp)
+        temp[0] = (data & 0xFF00) >> 8
+        self.bus.write_i2c_block_data(self.addr, address, temp)
 
     def set_calibration_32V_2A(self):
         """Configures to INA219 to be able to measure up to 32V and 2A of current. Counter
@@ -148,7 +152,7 @@ class INA219:
         # MaximumPower = 102.4W
 
         # Set Calibration register to 'Cal' calculated above
-        self.write(_REG_CALIBRATION,self._cal_value)
+        self.write(_REG_CALIBRATION, self._cal_value)
 
         # Set Config register to take into account the settings above
         self.bus_voltage_range = BusVoltageRange.RANGE_32V
@@ -161,17 +165,17 @@ class INA219:
                       self.bus_adc_resolution << 7 | \
                       self.shunt_adc_resolution << 3 | \
                       self.mode
-        self.write(_REG_CONFIG,self.config)
+        self.write(_REG_CONFIG, self.config)
 
     def getShuntVoltage_mV(self):
-        self.write(_REG_CALIBRATION,self._cal_value)
+        self.write(_REG_CALIBRATION, self._cal_value)
         value = self.read(_REG_SHUNTVOLTAGE)
         if value > 32767:
             value -= 65535
         return value * 0.01
 
     def getBusVoltage_V(self):
-        self.write(_REG_CALIBRATION,self._cal_value)
+        self.write(_REG_CALIBRATION, self._cal_value)
         self.read(_REG_BUSVOLTAGE)
         return (self.read(_REG_BUSVOLTAGE) >> 3) * 0.004
 
@@ -182,30 +186,31 @@ class INA219:
         return value * self._current_lsb
 
     def getPower_W(self):
-        self.write(_REG_CALIBRATION,self._cal_value)
+        self.write(_REG_CALIBRATION, self._cal_value)
         value = self.read(_REG_POWER)
         if value > 32767:
             value -= 65535
         return value * self._power_lsb
-        
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
 
     # Create an INA219 instance.
     ina219 = INA219(addr=0x41)
     while True:
-        bus_voltage = ina219.getBusVoltage_V()             # voltage on V- (load side)
-        shunt_voltage = ina219.getShuntVoltage_mV() / 1000 # voltage between V+ and V- across the shunt
-        current = ina219.getCurrent_mA()                   # current in mA
-        power = ina219.getPower_W()                        # power in W
-        p = (bus_voltage - 9)/3.6*100
-        if(p > 100):p = 100
-        if(p < 0):p = 0
+        bus_voltage = ina219.getBusVoltage_V()  # voltage on V- (load side)
+        shunt_voltage = ina219.getShuntVoltage_mV() / 1000  # voltage between V+ and V- across the shunt
+        current = ina219.getCurrent_mA()  # current in mA
+        power = ina219.getPower_W()  # power in W
+        p = (bus_voltage - 9) / 3.6 * 100
+        if (p > 100): p = 100
+        if (p < 0): p = 0
 
         # INA219 measure bus voltage on the load side. So PSU voltage = bus_voltage + shunt_voltage
-        #print("PSU Voltage:   {:6.3f} V".format(bus_voltage + shunt_voltage))
-        #print("Shunt Voltage: {:9.6f} V".format(shunt_voltage))
+        # print("PSU Voltage:   {:6.3f} V".format(bus_voltage + shunt_voltage))
+        # print("Shunt Voltage: {:9.6f} V".format(shunt_voltage))
         print("Load Voltage:  {:6.3f} V".format(bus_voltage))
-        print("Current:       {:9.6f} A".format(current/1000))
+        print("Current:       {:9.6f} A".format(current / 1000))
         print("Power:         {:6.3f} W".format(power))
         print("Percent:       {:3.1f}%".format(p))
         print("")
